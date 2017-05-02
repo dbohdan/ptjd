2d04825bd273f56ed9112badef8dc5b7b8a16d28
dbohdan <dbohdan@dbohdan.com>
2017-05-02 16:29:11 +0300

    benchmark.tcl: Report engine ("Tcl" or "Jim")

--- benchmark.tcl
+++ benchmark.tcl
@@ -13,13 +13,31 @@ proc read-test-file filename {
     return $result
 }
 
+proc engine {} {
+    if {[info exists ::tcl_platform(engine)]} {
+        return $::tcl_platform(engine)
+    } elseif {[string match 8.* [info patchlevel]]} {
+        return Tcl
+    } elseif {![catch {
+            proc foo {} {{bar 0}} {}; info statics foo; rename foo {}
+        }]} {
+        return Jim
+    } else {
+        return Unknown
+    }
+}
+
 proc main {} {
     set files [list grayscale.jpg landscape.jpg landscape-q.jpg]
+    puts $::debugChan "Running on [engine] [info patchlevel]"
     puts $::debugChan "$::iterations iterations per image"
+    puts $::debugChan ==========================
     foreach filename $files {
         set image [read-test-file $filename]
         lassign [time {::ptjd::decode $image} $::iterations] microseconds
-        puts $::debugChan "$filename: [expr {round($microseconds/1000)}] ms"
+        puts $::debugChan [format {%-16s %6i ms} \
+                                  $filename \
+                                  [expr {round($microseconds/1000)}]]
     }
 }
 
